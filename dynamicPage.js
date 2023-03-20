1467984199df889052449982c98f7c5d854c79cc
Evan <22979772@student.uwa.edu.au>
2023-03-20 12:01:40 +0800

    Revert "dynamic loading"
    This reverts commit 1506c74b40b5734a14800ea3ac2c4c945dbc19e2.

--- dynamicPage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#content").load("intro.html");
-    $("a").click(function () {
-        link = $(this).attr("href");
-        $("#content").load(link);
-        return false;
-    });
-});
-
